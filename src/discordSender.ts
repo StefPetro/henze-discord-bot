de0c1914242232cb8524481e10300811e0e859ee
Gustav Friis-Hansen <gustav.friis-hansen@fellowmind.dk>
2025-09-01 18:29:16 +0200

    Updated sender to always send to the first eligable text channel

--- src/discordSender.ts
+++ src/discordSender.ts
@@ -13,14 +13,14 @@ export const sendMessage = () => {
 
     const guilds = client.guilds.cache;
     guilds.forEach(guild => {
-      const generalTextChannel = guild.channels.cache.filter(channel => channel.name === "general" && channel.isTextBased()).first() as TextChannel;
+      const textChannel = guild.channels.cache.filter(channel => channel.isTextBased() && channel.isSendable()).first() as TextChannel;
       const tomorrow = new Date();
       tomorrow.setDate(tomorrow.getDate() + 1);
       const tomorrowString = tomorrow.toLocaleDateString("da-DK", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
-      console.log("Sending Henze-odds on server: " + guild.name);
+      console.log(`Sending Henze-odds on server: ${guild.name}, channel: ${textChannel.name}`);
 
-      generalTextChannel.send({
+      textChannel.send({
         content: `Henze-odds for i morgen: ${tomorrowString}`,
         files: [fileName]
       });
