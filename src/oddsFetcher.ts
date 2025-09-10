000ce77413df06f1d1318a38145b40a902167a19
Stefan Petrovic <43845091+StefPetro@users.noreply.github.com>
2025-09-10 16:48:54 +0200

    Update filepath to be the root folder

--- src/oddsFetcher.ts
+++ src/oddsFetcher.ts
@@ -5,7 +5,7 @@ export const writeHenzeOddsToFile = async () => {
   const response = await fetch("https://content.sb.danskespil.dk/content-service/api/v1/q/time-band-event-list?maxMarkets=9999&marketSortsIncluded=--,H1,H2,HH,HL,MH,MR,WH&marketGroupTypesIncluded=CUSTOM_GROUP,MATCH_RESULT,MONEYLINE,STATIC_SPREAD,STATIC_TOTAL&allowedEventSorts=MTCH&includeChildMarkets=true&prioritisePrimaryMarkets=true&includeCommentary=true&includeIncidents=true&includeMedia=true&drilldownTagIds=12&excludeDrilldownTagIds=20769,22796,22797,22800&maxTotalItems=120&maxEventsPerCompetition=99&maxCompetitionsPerSportPerBand=99&maxEventsForNextToGo=99&startTimeOffsetForNextToGo=600");
   const data = await response.json() as Root;
   const tomorrow = data.data.timeBandEvents.filter(x => x.type === "TOMORROW")?.[0];
-  const fileName = `../henze.txt`;
+  const fileName = `./henze.txt`;
   if (tomorrow?.events) {
     const henzeEvents = getHenzeOddsFromTimeBandEvents(tomorrow);
     console.log(`Total amount of Henze Events found: ${henzeEvents.length}`);
